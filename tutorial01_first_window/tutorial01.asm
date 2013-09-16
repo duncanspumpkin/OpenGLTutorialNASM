a7316485dba1e59c7585f879c9e9db3eb300902f
Duncan Frost <duncans_pumpkin@hotmail.co.uk>
2013-09-16 18:29:33 +0100

    Fixed crash due to missing window variable. Added esc to quit.

--- tutorial01_first_window/tutorial01.asm
+++ tutorial01_first_window/tutorial01.asm
@@ -4,6 +4,9 @@ extern glfwWindowHint
 extern glfwCreateWindow
 extern glfwTerminate
 extern glfwSwapBuffers
+extern glfwGetKey
+extern glfwWindowShouldClose
+extern glfwPollEvents
 extern glClearColor
 extern _glewInit@0 
 
@@ -13,6 +16,10 @@ import glfwWindowHint glfw3.dll
 import glfwCreateWindow glfw3.dll
 import glfwSwapBuffers glfw3.dll
 import glfwTerminate glfw3.dll
+import glfwGetKey glfw3.dll
+import glfwWindowShouldClose glfw3.dll
+import glfwPollEvents glfw3.dll
+
 import glClearColor opengl32.dll
 import _glewInit@0 glew32.dll
 
@@ -46,18 +53,34 @@ section .code use32 Class=CODE
   push dword 768
   push dword 1024
   call [glfwCreateWindow]
-  push dword eax
+  add  dword esp,52 ;clean up the stack from all these std calls
+  push eax
   call [_glewInit@0]
+
   push dword 0
-  push dword [ZP4] ;;;NOOO
+  push dword [ZP4] 
   push dword 0
   push dword 0
-  call [glClearColor]
+  call [glClearColor] ;note this is a cdecl call
   nop
  buffloop:
-  call [glfwSwapBuffers]
+  call [glfwPollEvents]
+  call [glfwSwapBuffers] ;as this is a std call and window is allready on stack
+  ;we dont need to give it any other params.
+  pop eax
+  push dword 256 ;;note renamed GLFW_KEY_ESCAPE
+  push eax
+  call [glfwGetKey]
+  sub dword eax,0
+  jnz terminate
+  pop eax
+  mov [esp],eax
+  call [glfwWindowShouldClose]
+  sub dword eax,0
+  jnz terminate
   jmp buffloop
   nop
+ terminate:
   call [glfwTerminate]
 
 
